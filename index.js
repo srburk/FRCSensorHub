a679844009abf94e134f9967f09cc43a6ca9f71f
Sam Burkhard <samryanburk@gmail.com>
2020-01-05 00:48:57 -0500

    Replicated time websocket server

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var WebSocket = require('ws').Server;
+
+var app = express();
+
+// initialize Websocket server
+ws = new WebSocket({ port: 8080 });
+
+// websocket connection event handler
+ws.on('connection', (socket, req) => {
+  // client information
+  const ip = req.connection.remoteAddress;
+
+  // status handler
+  socket.send('Connected to server via WebSocket');
+  console.log('Client connected:' + ip);
+
+  socket.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+// websocket broadcaster
+setInterval(() => {
+  ws.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
+
+// routing setup
+app.get('/', (req, res) => {
+    res.sendFile('./client/index.html', { root: __dirname });
+});
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, () => {
+  console.log('FRCSensorHub listening on port 3000');
+});
