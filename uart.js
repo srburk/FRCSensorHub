dd3eec715e5da787458a9c6b8302513f43b6707a
Sam Burkhard <samryanburk@gmail.com>
2020-01-09 18:26:38 -0500

    Added basic RS232 code

--- uart.js
+++ uart.js
@@ -1,6 +1,7 @@
 var SerialPort = require('serialport');
-var port = new SerialPort(path, { baudRate: 256000 });
+var port = new SerialPort('/dev/ttyAMA0', { baudRate: 9600 });
 
 setInterval(() => {
-  port.write('F*** apple');
-}, 1000);
+        port.write('Hello World');
+        console.log('Seems to be working');
+}, 5000);
