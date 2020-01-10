5807e9d5ec65d2054d8aeea7ca52846a18fa1fba
Sam Burkhard <samryanburk@gmail.com>
2020-01-09 20:04:07 -0500

    Removed testing uart code

--- uart.js
+++
@@ -1,7 +0,0 @@
-var SerialPort = require('serialport');
-var port = new SerialPort('/dev/ttyAMA0', { baudRate: 9600 });
-
-setInterval(() => {
-        port.write('Hello World');
-        console.log('Seems to be working');
-}, 5000);
