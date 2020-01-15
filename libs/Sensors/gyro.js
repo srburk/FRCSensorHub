94fe35f1d465589006adc395cb7d92738e4aab8d
Drew <dfriend414@gmail.com>
2020-01-14 22:49:50 -0500

    Created a library of the gyro commands and registers

--- libs/Sensors/gyro.js
+++ libs/Sensors/gyro.js
@@ -1,7 +1,13 @@
 //i2c address is 0x20
-exports.read = () => {
+exports.read = () => {           //Cartesian heading(0-359, unsigned, resets to 0 after 359)
+    var angle;
 
+    return angle;
 }
+
+// exports.read = () => {          //Absolute heading(positive or neagtive, signed, does not have a limit)
+// 
+// }
 
 exports.cal = () => {
 
