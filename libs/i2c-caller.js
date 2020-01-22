ad08402955743192df298d46b9b12e473e258de1
Drew <dfriend414@gmail.com>
2020-01-21 19:56:02 -0500

    fixed instatiation

--- libs/i2c-caller.js
+++ libs/i2c-caller.js
@@ -1,10 +1,10 @@
-var bus = require('i2c-bus');
+var i2cBus = require('i2c-bus');
 var gyro = require('./Sensors/gyro');
 var gyroAdd = 0x20;
 var converterAdd = 0x72;
 
 exports.setup = () => {
-    bus.openSync(0);
+    bus = i2cBus.openSync(0);
     gyro.calibrate(bus);
     gyro.reset(bus);
 }
