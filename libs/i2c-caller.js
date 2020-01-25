0cd2782396aed68d13f5c0eb675dfc8696d892ac
Drew <dfriend414@gmail.com>
2020-01-25 10:50:24 -0500

    changed bus number

--- libs/i2c-caller.js
+++ libs/i2c-caller.js
@@ -1,7 +1,7 @@
 const i2cBus = require(`i2c-bus`);
 const gyro = require(`./Sensors/gyro`);
 const converterAdd = 0x72;
-const bus = i2cBus.openSync(0);
+const bus = i2cBus.openSync(1);
 
 exports.setup = () => {
   gyro.calibrate(bus);
