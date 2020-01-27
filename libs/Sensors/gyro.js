f39b3cee4de5ad97ed0f57a6c54514a857dc9676
Drew <dfriend414@gmail.com>
2020-01-27 17:22:34 -0500

    cleaning up for merge

--- libs/Sensors/gyro.js
+++ libs/Sensors/gyro.js
@@ -3,14 +3,11 @@
 const register = require(`../Sensors/gyroRegisters.json`);
 const gyroAddress = 0x10;
 const calibrateBuffer = Buffer.from([0x03, 0x4E]);
-
 const resetBuffer = Buffer.from([0x03, 0x52]);
-
 const normalOperationBuffer = Buffer.from([0x03, 0x00]);
-
 const read_msb = 0x05;
-
 const read_lsb = 0x04;
+
 exports.read = (bus) => { // Cartesian heading(0-359, unsigned, resets to 0 after 359)
   let angle;
   let lsb = bus.readByteSync(gyroAddress, read_lsb);
@@ -21,7 +18,12 @@ exports.read = (bus) => { // Cartesian heading(0-359, unsigned, resets to 0 afte
 };
 
 // exports.read = (bus) => {          //Absolute heading(positive or neagtive, signed, does not have a limit)
-//
+//let angle;
+// let lsb = bus.readByteSync(gyroAddress, read_lsb);
+// let msb = bus.readByteSync(gyroAddress, read_msb);
+// msb <<= 8;
+// angle = msb + lsb;
+// return angle;
 // }
 
 exports.calibrate = (bus) => {
--- libs/i2c-caller.js
+++ libs/i2c-caller.js
@@ -1,6 +1,5 @@
 const i2cBus = require(`i2c-bus`);
 const gyro = require(`./Sensors/gyro`);
-const converterAdd = 0x72;
 const bus = i2cBus.openSync(1);
 
 exports.gyroCalibrate = () => {
